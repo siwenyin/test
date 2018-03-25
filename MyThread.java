7a7142435fc5a11b05f4ef5b908d2022fc2c7e75
siwenyin <siwenyin@tju.edu.cn>
2018-03-25 14:53:28 +0800

    宝贝宝贝吧
    宝贝宝贝

--- MyThread.java
+++ MyThread.java
@@ -1,7 +1,10 @@
 package mythread;
 
+public class MyThread extends Thread {
 	@Override
+	public void run() {
 		try {
+			for (int i = 0; i < 10; i++) {
 				int time = (int) (Math.random() * 1000);
 				Thread.sleep(time);
 				System.out.println("run=" + Thread.currentThread().getName());
